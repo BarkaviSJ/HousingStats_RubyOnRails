c3391dd056befdb89f6703554586e13551d623c4
BARKAVI SUNDARARAJAN <b.sundararajan.19@abdn.ac.uk>
2020-10-24 17:21:51 +0000

    added destroy table in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Location.destroy_all
 locations = File.read("lib/assets/housingfiles/locations.csv").lines 
 locations.shift # removing the headers from the csv file
-locations.each { |location|region, area = location.split(","); Location.create(region: region, area_code: area) }
+locations.each { |location|region, area, longitude, latitude = location.split(","); Location.create(region: region, area_code: area, longitude: longitude, latitude: latitude) }
